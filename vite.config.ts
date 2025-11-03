78e4dffb3bbd08078cbaa3e4b0f449a18bc0bfe8
Tuana Bıçakcı <bicakcituana@gmail.com>
2025-11-03 22:49:24 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/<anger-dileklen-bloomberg>/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
