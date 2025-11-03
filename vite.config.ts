c477fea7fae0cd6688761dfdfa6ec81c2d2c698c
Tuana Bıçakcı <bicakcituana@gmail.com>
2025-11-03 22:52:54 +0100

    Add vite-tsconfig-paths plugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),tsconfigPaths(),
   base: '/<anger-dileklen-bloomberg>/',
   resolve: {
     alias: {
